a6adc778d47aea95af3794cca9f8e6f02b81b0fa
jodosjodos <jeandedieu2030@gmail.com>
2024-05-01 20:45:43 +0200

    [add] add more compoents

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:te_lead/pages/landing_page.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
--- lib/pages/signin-signup/sign_up_page.dart
+++ lib/pages/signin-signup/sign_up_page.dart
@@ -1,39 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  bool hidden = true;
+  void togglePasswordState() {
+    setState(() {
+      hidden = !hidden;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    hidden = !hidden;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 50,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 100,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(child: Image.asset("assets/images/smallLogo.png")),
+              Text(
+                "Getting Started.!",
+                textAlign: TextAlign.start,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                "Create an Account to Continue your allCourses",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  hintStyle: Theme.of(context).textTheme.titleSmall,
+                  prefixIcon: const Icon(
+                    Icons.email_outlined,
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextField(
+                obscureText: hidden,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  hintStyle: Theme.of(context).textTheme.titleSmall,
+                  prefixIcon: const Icon(
+                    Icons.lock_outline,
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: togglePasswordState,
+                    icon: hidden
+                        ? const Icon(Icons.visibility_off_outlined)
+                        : const Icon(Icons.visibility_outlined),
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Image.asset("assets/images/agree2.png"),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: Text(
+                      "Agree to Terms & Conditions",
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(child: Image.asset("assets/images/smallLogo.png")),
-            Text(
-              "Getting Started.!",
-              textAlign: TextAlign.start,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Text(
-              "Create an Account to Continue your allCourses",
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-            const SizedBox(
-              height: 20,
-            )
-          ],
-        ),
-      )),
+      ),
     );
   }
 }
