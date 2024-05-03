43cc3508686027fbd1433ddc7f84d69dfac96f6e
jodosjodos <jeandedieu2030@gmail.com>
2024-05-03 14:16:59 +0200

    [add] add more fields in fill your profile

--- lib/pages/fill_profile.dart
+++ lib/pages/fill_profile.dart
@@ -12,23 +12,27 @@ class FillProfile extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.background,
       ),
       body: SafeArea(
-        child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
               Stack(
                 children: [
                   CircleAvatar(
-                    radius: 100,
+                    radius: 50,
                     child: SvgPicture.asset("assets/images/profile.svg",
-                        height: 200, width: 200),
+                        height: 100, width: 100),
                   ),
                   Positioned(
-                    bottom: 20,
-                    right: 0,
+                    bottom: 2,
+                    height: 30,
+                    right: 3,
                     child: Container(
                       decoration: const BoxDecoration(
-                          color: Colors.green, shape: BoxShape.circle),
+                          color: Color.fromRGBO(22, 127, 113, 1),
+                          shape: BoxShape.circle),
                       child: IconButton(
+                        iconSize: 20,
                         color: Colors.white,
                         icon: const Icon(
                           Icons.edit_calendar_outlined,
@@ -38,7 +42,73 @@ class FillProfile extends StatelessWidget {
                     ),
                   )
                 ],
-              )
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextField(
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  hintText: "Full Name",
+                  hintStyle: Theme.of(context).textTheme.titleSmall,
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  hintText: "Nick Name",
+                  hintStyle: Theme.of(context).textTheme.titleSmall,
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ShowDate(
+                keyboardType: TextInputType.datetime,
+                decoration: InputDecoration(
+                  hintText: "Date of Birth",
+                  hintStyle: Theme.of(context).textTheme.titleSmall,
+                  prefixIcon: const Icon(Icons.calendar_month_outlined),
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  hintStyle: Theme.of(context).textTheme.titleSmall,
+                  prefixIcon: const Icon(Icons.email_outlined),
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none),
+                ),
+              ),
             ],
           ),
         ),
